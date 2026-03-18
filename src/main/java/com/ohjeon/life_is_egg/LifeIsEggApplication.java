e530e59457fb48b28ff9d17da5e27979602607c0
OHJOUYEON <hyperclutter@gmail.com>
2026-03-18 18:39:50 +0900

    chore: init project

---
+++ src/main/java/com/ohjeon/life_is_egg/LifeIsEggApplication.java
@@ -0,0 +1,13 @@
+package com.ohjeon.life_is_egg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeIsEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeIsEggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohjeon/life_is_egg/LifeIsEggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohjeon.life_is_egg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeIsEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
